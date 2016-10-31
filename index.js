df1aa099c5684acdbd40eda2f20028428a2f2ee6
Gareth Visagie <gareth@gjvis.com>
2016-10-31 15:45:42 +0000

    feat: implement functions

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const api = {};
 
 Object.assign(api, require('./lib/comparison'));
 Object.assign(api, require('./lib/ranges'));
+Object.assign(api, require('./lib/functions'));
 
 module.exports = api;
